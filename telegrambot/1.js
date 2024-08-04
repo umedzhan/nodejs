81c8f759d215d0b2c695f9708fa7b3d6ec649c39
Umedjon <umedjon@kali>
2024-08-04 08:03:12 +0500

    1.js

---
+++ telegrambot/1.js
@@ -0,0 +1,23 @@
+//npm install node-telegram-bot-api
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// Sizning tokeningizni bu yerga qo'ying
+const token = '7391129165:AAHVANAucuSGZcdxnV54soRrZ_jhWsKq8VM';
+
+// Botni yaratish (polling usulida ishlash)
+const bot = new TelegramBot(token, { polling: true });
+
+// /start komandasiga javob berish
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Salom! Men sizning botim.');
+});
+
+// Har qanday matnga javob berish
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  if (msg.text.toLowerCase() !== '/start') {
+    bot.sendMessage(chatId, 'Siz xabar yubordingiz: ' + msg.text);
+  }
+});
